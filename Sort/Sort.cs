176fbc8c5815735429ded5d697885f2318b4aa1e
DerrickDW <262203929+DerrickDW@users.noreply.github.com>
2026-04-01 14:45:51 -0500

    working on it

--- Sort/Sort.cs
+++ Sort/Sort.cs
@@ -1,17 +1,30 @@
 ﻿using System.Linq;
+using System.Text;
 namespace MONKEYTOOLS.Sort;
 
 public class Sort
 {
     public static void Run(string[] args)
     {
-        Console.WriteLine("MONKEY SORT");
-        Console.WriteLine("🚬🐒 Monkey Will Sort Your Media Files");
-        Console.WriteLine("Type 'help' for more information");
-        Console.WriteLine("Type 'exit' to quit");
         if (args.Length == 0)
         {
-            Console.WriteLine("Usage: money sort --all, --tv --movie");
+            Console.WriteLine("MONKEY SORT");
+            Console.WriteLine("🚬🐒 Monkey Will Sort Your Media Files");
+            Console.WriteLine("Type 'help' for more information");
+            Console.WriteLine("Type 'exit' to quit");
+            Console.WriteLine("Usage: money sort <folder> --all, --tv --movie");
+            return;
+        }
+        
+        string root = args[0];
+        if (!Directory.Exists(root))
+        {
+            Console.WriteLine(" Folder does not exist");
+        }
+
+        foreach (var file in Directory.GetFiles(root))
+        {
+            HandleFile(file, root);
         }
     }
 }
